9abf440b57869aa7a91ed7f2cda164a2f4af7b0c
O <debragalinc@gmail.com>
2017-03-21 14:27:02 -0700

    updated application.js with airtable api call and key

--- andrew-app/app/adapters/application.js
+++ andrew-app/app/adapters/application.js
@@ -1,4 +1,14 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
+
+
+	host: 'https://api.airtable.com',
+
+	    namespace: 'v0/appJKocZsUWpTuPv1',
+
+	    headers: {
+	    'Accept': 'application/json',
+		'Authorization': 'Bearer ' + 'keynFPXh4Lf94Zj74'
+		},
 });
