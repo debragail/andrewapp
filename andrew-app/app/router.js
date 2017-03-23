150e6a16866ecbd5f1e125e6cdf31a3f888557dd
O <debragalinc@gmail.com>
2017-03-23 13:41:53 -0700

    div class footer

--- andrew-app/app/router.js
+++ andrew-app/app/router.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import config from './config/environment';
-import googlePageview from './mixins/google-pageview';
 
 const Router = Ember.Router.extend({
   location: config.locationType
