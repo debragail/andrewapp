b167d4bcac7cb97a44f2290647bb130100d9142b
O <debragalinc@gmail.com>
2017-03-26 15:39:35 -0700

    test.js route

---
+++ app/routes/test.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+let test = [{
+	id: 'some-id',
+	who: 'andrew wk',
+	when: '2012',
+	what 'lighting and photography'
+}];
+	
+export default Ember.Route.extend({
+	model() {
+	    return item;
+	}
+});
