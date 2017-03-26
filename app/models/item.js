a65afec709ce281ade52522a2fadf53f032b3cb0
O <debragalinc@gmail.com>
2017-03-26 15:32:49 -0700

    edit item.js

--- app/models/item.js
+++ app/models/item.js
@@ -1,10 +1,18 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	    title: DS.attr('string'),
-	    who: DS.attr('string'),
+	queryParams: {
+	    who: {
+		replace: true
+		    }
+	}
+	    
+	    
+	    // static
+
+	    /*who: DS.attr('string'),
 	    desc: DS.attr('string'),
 	    when: DS.attr('date'),
 	    what: DS.attr('string'),
-	    url: DS.attr('string'),
+	    url: DS.attr('string'),*/
 });
